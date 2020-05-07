8ddfc748cc1b51b52628e8db95000c839bed9dc3
radoneje <den.shevchenko@gmail.com>
2020-05-07 19:55:01 +0300

    ffefe

--- public/javascripts/external.js
+++ public/javascripts/external.js
@@ -454,35 +454,7 @@ window.onload=async ()=> {
 
     }
 
-    async function createAudioAnaliser(stream, clbk) {
-        try {
-            audioContext = new AudioContext();
-            analyser = audioContext.createAnalyser();
-            microphone = audioContext.createMediaStreamSource(stream);
-            javascriptNode = audioContext.createScriptProcessor(2048, 1, 1);
-            analyser.smoothingTimeConstant = 0.8;
-            analyser.fftSize = 1024;
-            microphone.connect(analyser);
-            analyser.connect(javascriptNode);
-            javascriptNode.connect(audioContext.destination);
-            javascriptNode.onaudioprocess = function () {
-                var array = new Uint8Array(analyser.frequencyBinCount);
-                analyser.getByteFrequencyData(array);
-                var values = 0;
-                var length = array.length;
-                for (var i = 0; i < length; i++) {
-                    values += (array[i]);
-                }
-                var average = values / length;
-                //console.log(Math.round(average ));
-                clbk(average)
-            }
 
-            return audioContext;
-        } catch (e) {
-            return null
-        }
-    }
 
     function draw(v, c, videoTrack, img) {
 
@@ -627,6 +599,35 @@ window.onload=async ()=> {
             })
 
 
+    }
+    async function createAudioAnaliser(stream, clbk) {
+        try {
+            audioContext = new AudioContext();
+            analyser = audioContext.createAnalyser();
+            microphone = audioContext.createMediaStreamSource(stream);
+            javascriptNode = audioContext.createScriptProcessor(2048, 1, 1);
+            analyser.smoothingTimeConstant = 0.8;
+            analyser.fftSize = 1024;
+            microphone.connect(analyser);
+            analyser.connect(javascriptNode);
+            javascriptNode.connect(audioContext.destination);
+            javascriptNode.onaudioprocess = function () {
+                var array = new Uint8Array(analyser.frequencyBinCount);
+                analyser.getByteFrequencyData(array);
+                var values = 0;
+                var length = array.length;
+                for (var i = 0; i < length; i++) {
+                    values += (array[i]);
+                }
+                var average = values / length;
+                //console.log(Math.round(average ));
+                clbk(average)
+            }
+
+            return audioContext;
+        } catch (e) {
+            return null
+        }
     }
 }
 
